d7db60de34c33481cfdb44482fe23bd18eb65cb3
Loma <lomakim@gmail.com>
2026-02-05 18:31:32 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -8,13 +8,27 @@ struct Restaurant {
     int rating;
 };
 
-Restaurant addRestaurant();
+Restaurant addRestaurant(Restaurant&);
 
 int main(){
 
     return 0;
 }
 
-Restaurant addRestaurant(){
-    
+Restaurant addRestaurant(Restaurant &r){
+    cout << "Please enter the following information about the restaurant:\n";
+    cout << "\tName: ";
+    cin >> r.name;
+    cout << "\tCuisine: ";
+    cin >> r.type;
+    cout << "\tAddress: ";
+    cin >> address;
+    cout << "\tCity: ";
+    cin >> city;
+    cout << "\tState:";
+    cin >> state;
+    cout << "\tRating out of five: ";
+    cin >> rating;
+
+    return r;
 }
