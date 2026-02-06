2dc118079c9c9ea7db121b735c6c3c2c80cbddfc
Loma <lomakim@gmail.com>
2026-02-05 18:18:04 -0800

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// COMSC-210 | Lab 3A | Loma Kim
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Restaurant {
+    string name, type, address, city, state;
+    int rating;
+};
+
+Restaurant addRestaurant();
+
+int main(){
+
+    return 0;
+}
+
+Restaurant addRestaurant(){
+    
+}
