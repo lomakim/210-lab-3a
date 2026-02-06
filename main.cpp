609c3ab4526fe9ad2e0a56b1836b060da8cd347e
Loma <lomakim@gmail.com>
2026-02-05 19:07:28 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 // COMSC-210 | Lab 3A | Loma Kim
+/*  This program collects and stores input from the user about a
+    Restaurant they have been tothen outputs the information    */
+
 #include <iostream>
 #include <string>
 using namespace std;
 
+// Restaurant struct stores name, cuisine type, address, and rating
 struct Restaurant {
     string name, type, address, city, state;
     double rating;
+    int price;
 };
 
 Restaurant addRestaurant();
@@ -38,8 +43,16 @@ Restaurant addRestaurant() {
     cout << "\tState: ";
     getline (cin, r.state);
 
-    cout << "\tRating out of five: ";
+    cout << "\tRating out of 5: ";
     cin >> r.rating;
+    if (r.rating > 5 || r.rating < 0) {
+        cout << "\tError! You entered a number out of bounds.\n";
+        cout << "\tPlease enter a rating 1-5: ";
+        cin >> r.rating
+    }
+
+    cout << "\tPrice out of 3: ";
+    cin >> r.price;
 
     return r;
 }
